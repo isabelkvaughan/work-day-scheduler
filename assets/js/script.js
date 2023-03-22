b90add193d4e356c5d6af97b9950d1e6b290d466
Isabel <isabelkvaughan@Isabels-MacBook-Pro.local>
2023-03-23 09:56:09 +1100

    timeblocks created using js, current tiem added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,57 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-});
+$(document).ready(function(){
+//set date
+var today = dayjs().format("dddd DD MMMM, YYYY");
+  $('#currentDay').text("Today's date: " + today);
+
+//set time
+setInterval(function() {
+  var time = dayjs().format("h:mm:ss a");
+  $('#currentTime').text("Current time: " + time);
+}, 1000);
+
+createTimeBlocks()
+
+
+// define function to create time blocks
+function createTimeBlocks() {
+
+  //define start and end hour of the day
+  var startHr = 9;
+  var endHr = 17;
+
+  // loop through hours to create individual time blocks
+  for (var hour = startHr; hour <= endHr; hour++) {
+      
+      //create timeBlockEl
+      var timeBlockEl = document.createElement("div");
+      timeBlockEl.setAttribute("id", "hour-" + hour);
+      timeBlockEl.classList.add("row", "time-block");
+
+      // add hourEl and append to timeBlock
+      var hourEl = document.createElement("div");
+      hourEl.classList.add("col-2", "col-md-1", "hour", "text-center", "py-3");
+      hourEl.textContent = hour <= 12 ? hour + "AM" : hour - 12 + "PM";
+      timeBlockEl.appendChild(hourEl);
+
+      // add textInput and append to timeBlock
+      var textInput = document.createElement("textarea");
+      textInput.classList.add("col-8", "col-md-10", "description");
+      textInput.setAttribute("rows", "3");
+      timeBlockEl.appendChild(textInput);
+
+      // add saveBtn and append to timeBlock
+      var saveBtn = document.createElement("button");
+      saveBtn.classList.add("btn", "saveBtn", "col-2", "col-md-1");
+      saveBtn.setAttribute("aria-label", "save");
+      var saveIcon = document.createElement("i");
+      saveIcon.classList.add("fas", "fa-save");
+      saveIcon.setAttribute("aria-hidden", "true");
+      saveBtn.appendChild(saveIcon);
+      timeBlockEl.appendChild(saveBtn);
+
+      // Add the time-block element to the page
+      document.querySelector("#time-blocks").appendChild(timeBlockEl);
+  }
+
+
+}});
